4d33a68299cf36490f5c9e12945c0f3b28b09033
yazzya <yazzya@naver.com>
2018-05-11 16:34:29 +0900

    초기 세팅2

--- mobile_pungmu/src/main/java/com/pungmu/m/MobilePungmuApplication.java
+++ mobile_pungmu/src/main/java/com/pungmu/m/MobilePungmuApplication.java
@@ -2,9 +2,14 @@ package com.pungmu.m;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class MobilePungmuApplication {
+    
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MobilePungmuApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(MobilePungmuApplication.class, args);
